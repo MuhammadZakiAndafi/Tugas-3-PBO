cf31fbe71c30cdd6104ee0f05e7b1eec0cfa16e2
MuhammadZakiAndafi <144996436+MuhammadZakiAndafi@users.noreply.github.com>
2023-11-30 00:20:00 +0700

    Update App.java

--- Tugas-3-PBO/App.java
+++ Tugas-3-PBO/App.java
@@ -3,6 +3,8 @@ import java.util.InputMismatchException;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        dataPembelian zaki = new faktur(); 
+
         zaki.isidata();
         zaki.cetakfaktur();
 
